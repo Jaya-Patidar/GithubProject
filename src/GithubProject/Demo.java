8f9f1fbcc980b04e2f5d91fd2722275519c02192
jpatidar <jpatidar@inlen8520023615.corp.capgemini.com>
2022-10-26 22:38:18 +0530

    First commit........

---
+++ src/GithubProject/Demo.java
@@ -0,0 +1,24 @@
+package GithubProject;
+
+import java.util.Scanner;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=2;
+		int b=3;
+		int c=a+b;
+		System.out.println(c);
+		int o;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number:");
+		int v=sc.nextInt();
+		int w=sc.nextInt();
+        o=v+w;
+        System.out.println(o);
+        
+
+	}
+
+}
