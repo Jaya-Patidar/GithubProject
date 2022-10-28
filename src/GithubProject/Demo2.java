52315cb4cd634099cca21f0af1c1953f373023b7
aschando <aschando@inlen8520023545.corp.capgemini.com>
2022-10-28 10:09:57 +0530

    Commiting to Branch

---
+++ src/GithubProject/Demo2.java
@@ -0,0 +1,12 @@
+package GithubProject;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int i=10;
+        int j=20;
+        System.out.println(i+j);
+	}
+
+}
